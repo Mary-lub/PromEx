26991d1e21fbc0d8ea523341c1ab81a660661e6b
Mariia <marya654@yandex.ru>
2024-05-01 20:37:21 +0300

    added Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы,
+// начиная с конца. Использовать рекурсию, не использовать циклы.
+
+void OutpetElementsInReverseOrder(int[] array, int i = 0)
+{
+      if (i < array.Length)
+      {
+            OutpetElementsInReverseOrder(array, i + 1);
+            Console.Write(array[i] + " ");
+      }
+}
+int[] array = { 1, 2, 3, 4, 5};
+OutpetElementsInReverseOrder(array);
